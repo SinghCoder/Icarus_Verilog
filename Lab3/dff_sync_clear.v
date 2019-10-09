92269fa1638dc4662f2bf911e7de47252468f5b9
HarpinderJotSingh <harpinderjot36@gmail.com>
2019-10-09 07:15:33 +0530

    gitignore added

--- Lab3/dff_sync_clear.v
+++ Lab3/dff_sync_clear.v
@@ -1,15 +1,18 @@
+`ifndef DFF_SYNC_CLEAR
+`define DFF_SYNC_CLEAR
 // module d flip flop with synchronus clear
 // clock is positive edge triggered
-module dff_sync_clear(d,clearb, clock, q);
-	input d, clearb, clock;
-	output q;
-	reg q;
-	
-	always @(posedge clock)
-		begin
-			if(!clearb)
-				q = d;
-			else
-				q = 1'b0;
-		end
-endmodule
+	module dff_sync_clear(d,clearb, clock, q);
+		input d, clearb, clock;
+		output q;
+		reg q;
+		
+		always @(posedge clock)
+			begin
+				if(clearb)
+					q = d;
+				else
+					q = 1'b0;
+			end
+	endmodule
+`endif
