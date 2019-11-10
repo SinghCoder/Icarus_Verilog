0dff4e2996018c2f0f3f541ded7c6bbc71cc8155
HarpinderJotSingh <harpinderjot36@gmail.com>
2019-11-10 12:24:49 +0530

    Writing to BHT done, getting some predictions, haven't analyzed yet for correctness

--- LabTestPractice/2k16-17/file.v
+++ LabTestPractice/2k16-17/file.v
@@ -8,6 +8,7 @@ module bht(
     output [1:0] outp,
     input [1:0] inp,
     input [9:0] address,
+    input clk,
     input write
 );
     wire [1:0] outp;
@@ -17,14 +18,17 @@ module bht(
         for(i = 0; i < 1024; i = i+1) begin
             bhtTable[i] = 2'b0;
         end
+        // outp = 2'b0;
     end
     
-    assign outp = bhtTable[address];
-
-    always @(write)
+    always @(negedge clk) begin
+        
         if(write == 1)
             bhtTable[address] = inp;
+    end 
 
+    assign outp = bhtTable[address];
+        // $display("address = %b, bhtTable[address] = %b",address, bhtTable[address]);
 endmodule
 
 module mux(
@@ -45,92 +49,107 @@ module mux(
 endmodule
 
 module predictor(
-    output [1:0] outpState,
-    input actOutcome,
+    output [1:0] nextState,
+    input taken,
     input clock
 );
 
-    reg [1:0] machineState, outpState;
+    reg [1:0] currState, nextState;
 
     initial 
-        machineState = `S0;
+        currState = `S0;
     
     always @(posedge clock) begin
-        case({machineState,actOutcome}) 
+        case({currState,taken}) 
 
-            {`S0,1'b0} :   machineState = `S0;
-            {`S0,1'b1} :   machineState = `S1;
+            {`S0,1'b0} :   nextState <= `S0;
+            {`S0,1'b1} :   nextState <= `S1;
 
-            {`S1,1'b0} :   machineState = `S0;
-            {`S1,1'b1} :   machineState = `S3;
+            {`S1,1'b0} :   nextState <= `S0;
+            {`S1,1'b1} :   nextState <= `S3;
 
-            {`S2,1'b0} :   machineState = `S0;
-            {`S2,1'b1} :   machineState = `S3;
+            {`S2,1'b0} :   nextState <= `S0;
+            {`S2,1'b1} :   nextState <= `S3;
 
-            {`S3,1'b0} :   machineState = `S2;
-            {`S3,1'b1} :   machineState = `S3;
+            {`S3,1'b0} :   nextState <= `S2;
+            {`S3,1'b1} :   nextState <= `S3;
 
-            default :   machineState = `S0;
+            default :   begin
+                            $display("insisde default case.., currState: %d, taken : %b", currState, taken);
+                            nextState <= `S0;
+                        end
         endcase
 
-        outpState = machineState;
+        currState <= nextState;
     end
 
 endmodule
 
 module intg(
-    output prevPredn,
+    output predn,
     input [9:0] address,
-    input actOutcome,
+    input taken,
     input clock
 );
-    reg prevPredn;
+    reg predn;
     initial 
-        prevPredn = 1'b0;
+        predn = 1'b0;
     
     wire [1:0] bhtOut1, bhtOut2;
     wire [1:0] nextState;
     wire [1:0] branchPredn;
 
-    wire writeBht1, writeBht2;
+    reg writeBht1, writeBht2;
 
-    assign writeBht2 = ~writeBht1;
+    initial begin
+        writeBht1 = 1'b0;
+        writeBht2 = 1'b0;
+    end
+    // writeBht2 = ~writeBht1;
 
-    assign writeBht1 = ~actOutcome;
+    // assign writeBht1 = ~taken;
 
-    bht bht1(.outp(bhtOut1), .inp(nextState), .address(address), .write(writeBht1));
-    bht bht2(.outp(bhtOut2), .inp(nextState), .address(address), .write(writeBht2));
+    bht bht1(.outp(bhtOut1), .inp(nextState), .address(address), .write(writeBht1), .clk(clock));
+    bht bht2(.outp(bhtOut2), .inp(nextState), .address(address), .write(writeBht2), .clk(clock));
 
-    mux mux1(.outp(branchPredn), .inp1(bhtOut1), .inp2(bhtOut2), .sel(prevPredn));
-    predictor p1(.outpState(nextState), .actOutcome(actOutcome), .clock(clock));
+    mux mux1(.outp(branchPredn), .inp1(bhtOut1), .inp2(bhtOut2), .sel(predn));
+    predictor p1(.nextState(nextState), .taken(taken), .clock(clock));
 
-    always @(branchPredn) begin
+    always @(branchPredn or nextState) begin
         case (branchPredn)
 
             `S0 : begin
-                    $display("Not taken");
-                    prevPredn = 1'b0;
+                        $display("S0: Not taken");
+                        predn = 1'b0;
+                        writeBht1 = 1'b1;
+                    #10  writeBht1 = 1'b0;
                   end
 
             `S1 : begin
-                    $display("Not taken");
-                    prevPredn = 1'b0;
+                        $display("S1: Not taken");
+                        predn = 1'b0;
+                        writeBht1 = 1'b1;
+                    #10  writeBht1 = 1'b0;
                   end
 
             `S2 : begin
-                    $display("Taken");
-                    prevPredn = 1'b1;
+                        $display("S2: Taken");
+                        predn = 1'b1;
+                        writeBht2 = 1'b1;
+                    #10  writeBht2 = 1'b0;
                   end
 
             `S3 : begin
-                    $display("Taken");
-                    prevPredn = 1'b1;
+                        $display("S3: Taken");
+                        predn = 1'b1;
+                        writeBht2 = 1'b1;
+                    #10  writeBht2 = 1'b0;
                   end
 
 
         default  : begin
                     $display("Not taken");
-                    prevPredn = 1'b0;
+                    predn = 1'b0;
                   end
         endcase
 
@@ -167,7 +186,7 @@ module tb_intg();
         #10  actualOutcome = 1'b1;
         #10  actualOutcome = 1'b1;
         #10  actualOutcome = 1'b0;
-        #10  $finish;
+        #50  $finish;
     end
 
 endmodule
